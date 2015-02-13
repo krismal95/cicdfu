6d6f6a4666d229abd1282ed3fccd53430cf18c60
devMauris <devmauris@gmail.com>
2015-02-13 15:10:33 +0300

    Removing...

--- common_jobs/common_job_script.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-sudo service jenkins stop
-
-sudo cp -rf ./kymir /var/lib/jenkins/jobs/kymir_temp
-sudo cp -rf ./111 /var/lib/jenkins/jobs/111_temp
-sudo cp -rf ./pr1 /var/lib/jenkins/jobs/pr1_temp
-
-sudo service jenkins start
