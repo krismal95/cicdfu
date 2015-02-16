8816775c2665609c2a4053cc03eff0c8e1f84e31
krismal95 <krismail95@gmail.com>
2015-02-16 13:51:04 +0300

    jobs folder user changem

--- common_job_script.sh
+++ common_job_script.sh
@@ -4,8 +4,9 @@ sudo service jenkins stop
 HOME_JENKINS=/var/lib/jenkins
 ABSOLUTE_FILENAME=`readlink -e "$0"`
 DIRECTORY=`dirname "$ABSOLUTE_FILENAME"` 
-
+sudo mkdir $HOME_JENKINS/jobs
 sudo cp -rp $DIRECTORY/common_jobs/* $HOME_JENKINS/jobs
 sudo chown -R jenkins:jenkins $HOME_JENKINS/jobs/*
+sudo chown -R jenkins:jenkins $HOME_JENKINS/jobs
 
 sudo service jenkins start
